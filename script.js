5a9872f6f6da8ef2227b94d8a722d4cd9f154ce4
Denis <hallo-boy@mail.ru>
2022-09-08 19:03:42 +0300

    комментарии

--- script.js
+++ script.js
@@ -1,45 +1,62 @@
+//находим кнопку и поле ввода, записываем их в переменные
 const btn = document.querySelector("#btn");
 const input = document.querySelector("input");
-const item = document.querySelector("#to_do_list");
 
-input.addEventListener('keydown', function(event) {
-    if (event.code == 'Enter') {
-        addItem(input.value);
-        input.value = "";
-    }
-  });
+//добавляем функцию keydown для добавления нового элемента по нажатию клавиши
+input.addEventListener("keydown", function (event) {
+  if (event.code == "Enter") {
+    addItem(input.value);
+    input.value = "";
+  }
+});
 
-btn.onclick =  () => {
+//функция onclick для добавления элемента по клику на button
+btn.onclick = () => {
   addItem(input.value);
   input.value = "";
 };
 
+//функция создания и добавления нового элемента
 let addItem = function (input) {
+    //создаём див который будем добавлять
   const item = document.createElement("div");
+  //создаём див с иконками
   const div = document.createElement("div");
+  //создаём иконки
   const checkIcon = document.createElement("i");
   const trashIcon = document.createElement("i");
+  //создаём параграф в который будет помещено текстовое содержимое
   const paragraph = document.createElement("p");
 
+  //добавляем нашему элементу класс со стилями
   item.className = "item";
+  //добавляем параграфу текстовое содержимое из поля ввода
   paragraph.textContent = input;
 
+  //добавляем иконку галочки
   checkIcon.className = "fa-regular fa-circle-check";
+  //по клику на галочку она становится зеленой
   checkIcon.onclick = () => {
-checkIcon.style.color = "#00ff00";
+    checkIcon.style.color = "#00ff00";
   };
+  //добавляем галочку в див с иконками
   div.appendChild(checkIcon);
-   
+
+  //добавляем иконку корзины
   trashIcon.className = "fa-solid fa-trash";
+  //по клику на иконку пропадает созданный элемент
   trashIcon.onclick = () => {
     item.remove();
   };
-
-  div.className = "icons";
+ //добавляем корзину в див с иконками
   div.appendChild(trashIcon);
-
+  //добавим диву класс, чтобы немного поменять расположение иконок
+  div.className = "icons";
+  
+  //добавляем к нашему айтему текст и иконки
   item.appendChild(paragraph);
   item.appendChild(div);
 
-  document.querySelector('#to_do_list').append(item);
+  //добавляем айтем в HTML
+  document.querySelector("#to_do_list").append(item);
 };
